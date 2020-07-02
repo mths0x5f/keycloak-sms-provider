02fb55f16ede35a2bc42c67e11adb77ba907b9d1
Matheus <mths0x5f@users.noreply.github.com>
2020-07-01 21:13:39 -0300

    new required action to activate sms 2fa

---
+++ keycloak-sms-provider/src/main/java/io/github/mths0x5f/keycloak/requiredactions/sms/ConfigSmsOtpRequiredAction.java
@@ -0,0 +1,58 @@
+package io.github.mths0x5f.keycloak.requiredactions.sms;
+
+import io.github.mths0x5f.keycloak.authenticators.sms.SmsOtpCredentialProvider;
+import io.github.mths0x5f.keycloak.authenticators.sms.credential.SmsOtpCredentialModel;
+import io.github.mths0x5f.keycloak.providers.sms.spi.TokenCodeService;
+import org.keycloak.authentication.RequiredActionContext;
+import org.keycloak.authentication.RequiredActionProvider;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.core.Response;
+
+public class ConfigSmsOtpRequiredAction implements RequiredActionProvider {
+
+    public static final String PROVIDER_ID = "CONFIGURE_SMS_OTP";
+
+    @Override
+    public void evaluateTriggers(RequiredActionContext context) {
+    }
+
+    @Override
+    public void requiredActionChallenge(RequiredActionContext context) {
+        Response challenge = context.form()
+                .createForm("login-sms-otp-config.ftl");
+        context.challenge(challenge);
+    }
+
+    @Override
+    public void processAction(RequiredActionContext context) {
+        TokenCodeService tokenCodeService = context.getSession().getProvider(TokenCodeService.class);
+        String phoneNumber = context.getHttpRequest().getDecodedFormParameters().getFirst("phoneNumber");
+        String code = context.getHttpRequest().getDecodedFormParameters().getFirst("code");
+        try {
+            tokenCodeService.validateCode(context.getUser(), phoneNumber, code);
+            SmsOtpCredentialProvider socp = context.getSession().getProvider(SmsOtpCredentialProvider.class);
+            socp.createCredential(context.getRealm(), context.getUser(), SmsOtpCredentialModel.create(phoneNumber));
+            context.success();
+        } catch (BadRequestException e) {
+
+            Response challenge = context.form()
+                    .setError("noOngoingVerificationProcess")
+                    .createForm("login-sms-otp-config.ftl");
+            context.challenge(challenge);
+
+        } catch (ForbiddenException e) {
+
+            Response challenge = context.form()
+                    .setAttribute("phoneNumber", phoneNumber)
+                    .setError("verificationCodeDoesNotMatch")
+                    .createForm("login-update-phone-number.ftl");
+            context.challenge(challenge);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
